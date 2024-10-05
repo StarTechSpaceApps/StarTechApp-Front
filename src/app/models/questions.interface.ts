84326ff4ec6384eac47c3df90a5ec765c268f85e
Elisenda-LV <elisenda.lv@gmail.com>
2024-10-05 15:09:14 +0200

    interface added

--- src/app/models/questions.interface.ts
+++ src/app/models/questions.interface.ts
@@ -1,3 +1,12 @@
-export interface Question {
-
+export interface IQuestion {
+  id: string,
+  statment: string,
+  correctAnswer: string,
+  wrongAnswer: string,
+  correctImage: string,
+  wrongImange: string,
+  answerText: string,
+  answerLink: string;
+  audience: string; //enum
+  topic: string //enum
 }
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,14 +1,14 @@
 import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Question } from '../models/questions.interface';
+import { IQuestion } from '../models/questions.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  public question: Question[] = [];
+  public question: IQuestion[] = [];
   public hhtpClient = Inject(HttpClient);
   //añadir url desde environment
 
