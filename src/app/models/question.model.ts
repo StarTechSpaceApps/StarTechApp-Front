bc3ccbc9bd6fa57e72c10059254e38f4a10b3067
Elisenda-LV <elisenda.lv@gmail.com>
2024-10-05 17:42:06 +0200

    model update

--- src/app/models/question.model.ts
+++ src/app/models/question.model.ts
@@ -2,11 +2,11 @@ import { AnswerSchema } from './answer.interface';
 import { ExplanationSchema } from './explanation.interface';
 
 export interface Question {
-  id: string;
   statement: string;
   correctAnswer: AnswerSchema;
   wrongAnswer: AnswerSchema;
   explanation: ExplanationSchema;
   audience: string;
   topic: string;
+  id: string;
 }
