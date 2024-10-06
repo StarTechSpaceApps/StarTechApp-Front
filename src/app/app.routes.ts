087b50b8fc9a64b45d9a582191d49c8a976ac0a0
Elisenda-LV <elisenda.lv@gmail.com>
2024-10-06 13:23:18 +0200

    update logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    title: 'Star Tech Space App',
+    title: 'Goals for Good',
     loadComponent: () => import('./components/home/home.component').then(c => c.HomeComponent),
 
   },
   {
     path: 'question',
-    title: 'quiz',
+    title: 'Goals for Good',
     loadComponent: () => import('./components/quiz/quiz.component').then(c => c.QuizComponent),
 
   },
   {
     path: 'question/:id',
-    title: 'answer',
+    title: 'Goals for Good',
     loadComponent: () => import('./components/quiz/answer/answer.component').then(c => c.AnswerComponent),
 
   },
